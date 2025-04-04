3fe6e177a9a99d06e73ed95d8826485cda49314e
Levhita <armando.arias@incode.com>
2025-04-04 16:20:14 -0600

    * the component show, but fails

--- src/App.js
+++ src/App.js
@@ -6,47 +6,31 @@ https://developer.incode.com/docs/tutorial-creating-an-identity-validation-app
 */
 
 import React, { useEffect, useState, useRef } from "react";
+import { renderCaptureId } from "@incodetech/welcome";
 import { incode } from "./incode";
-import { fakeBackendStart, fakeBackendFinish } from './fake_backend'
 import "./App.css";
 
 let incodeSession;
 let container;
 
-function saveDeviceData() {
-  incode.sendGeolocation({ token: incodeSession.token });
-  incode.sendFingerprint({ token: incodeSession.token });
-}
-
-function captureIdFrontSide() {
-  incode.renderCamera("front", container, {
-    onSuccess: captureIdBackSide,
-    onError: console.log,
-    token: incodeSession,
-    numberOfTries: 3,
-    showTutorial: true,
-  });
-}
-
-function captureIdBackSide() {
-  incode.renderCamera("back", container, {
+function captureId() {
+  renderCaptureId(container, {
     onSuccess: processId,
     onError: console.log,
-    token: incodeSession,
-    numberOfTries: 3,
-    showTutorial: true,
+    session: incodeSession,
+    //forceIdV2: true 
   });
 }
 
 function processId() {
   return incode
-  .processId({ token: incodeSession.token })
-  .then(() => {
-    captureSelfie();
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+    .processId({ token: incodeSession.token })
+    .then(() => {
+      captureSelfie();
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 }
 
 function captureSelfie() {
@@ -60,51 +44,45 @@ function captureSelfie() {
 }
 
 function finishOnboarding() {
-  fakeBackendFinish(incodeSession.token).then(() => {
-    console.log("Onboarding Finished");
-    container.innerHTML="Onboarding Finished";
-  });
+  container.innerHTML = "Onboarding Finished";
 }
 
 function App() {
   const [session, setSession] = useState(null); // Stores the Session
   const incodeContainerRef = useRef(null);
-  
+
   // Store data that will not trigger re-renders unless specifically told so
-  const isLoaded = useRef(false); 
-  
+  const isLoaded = useRef(false);
+
   // Run this after the initial loading
   useEffect(() => {
     // Only fetch the data if we havent fetched it yet
     if (isLoaded.current) return;
-    
-    //Fetch the session and save it on the session variable
-    fakeBackendStart().then(async (session) => {
+
+    incode
+    .createSession("ALL", null, {
+      configurationId: process.env.REACT_APP_BACKEND_FLOWID,
+    })
+    .then((session) => {
       setSession(session);
-    }).catch(
-      (e)=>console.log(e)
-    );
-    
+    });
+
     // We already sent the async call, don't call it again
     isLoaded.current = true;
   }, []);
-  
-  useEffect(()=>{
+
+  useEffect(() => {
     container = incodeContainerRef.current;
-    if(session){
-      incodeSession=session;
-      saveDeviceData();
-      captureIdFrontSide();
-    };
-  }, [session])
- 
+    if (session) {
+      incodeSession = session;
+      captureId();
+    }
+  }, [session]);
+
   return (
     <div className="App">
-    <div ref={incodeContainerRef}></div>
-    {!session && (
-      <p>Starting session...</p>
-    )}
-    <div ref={incodeContainerRef}></div>
+      <div ref={incodeContainerRef}></div>
+      {!session && <p>Starting session...</p>}
     </div>
   );
 }
--- src/incode.js
+++ src/incode.js
@@ -1,9 +1,11 @@
 import { create } from "@incodetech/welcome";
 import translations from "./translations";
 const apiURL = process.env.REACT_APP_INCODE_API_URL;
+const apiKey = process.env.REACT_APP_BACKEND_APIKEY;
 
 const incode = create({
   apiURL: apiURL,
+  apiKey: apiKey,
   lang: 'en-US',
   translations
 });
